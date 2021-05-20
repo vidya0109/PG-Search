5a5237eec91ee71b5477238eea50555a94cb4098
Dibyaroopdas <59794247+Dibyaroopdas@users.noreply.github.com>
2021-05-21 01:03:35 +0530

    Commit Third

---
+++ app/src/main/java/com/example/pgsearch/SearchActivty.java
@@ -0,0 +1,14 @@
+package com.example.pgsearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_activty);
+    }
+}
